de397e055820d1f6452dbaafc821b8d9720d437f
Katrina <katrina.cullen@manchester.ac.uk>
2019-03-26 13:14:13 +0000

    commit commit commit again...

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+# import the flask module form the flask library
 from flask import Flask, render_template
 
+# assign name 'app' to flask app
 app = Flask(__name__)
 
+# render specified html files for given URL addresses
 @app.route('/home')
 def home():
     return render_template('home.html')
@@ -14,5 +17,6 @@ def katrina():
 def sarah():
     return render_template('sarah.html')
 
+# run the app with debug mode on if name = __main__  (the __Main__ is a convention)
 if __name__ == '__main__':
     app.run(debug=True)
